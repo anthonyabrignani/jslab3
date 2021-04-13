5187ae6d04f585e98e823cd1bdaf77477763b684
anthony abrignani <anthony abrignani>
2021-04-13 15:21:20 -0400

    cleaned first commit

--- script.js
+++
@@ -1,9 +0,0 @@
-const newArray = [2, 5, 9, 3, 20, 6];
-
-const isGreaterThan = (number) => number > 6;
-
-console.log(newArray.every(isGreaterThan));
-
-const findBiggest = (number) => number > 10;
-
-console.log(newArray.find(findBiggest));
